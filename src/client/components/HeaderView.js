2dd288e259e61b81f2898c07d3a53bcd8cc0ac58
fpena06 <fpena06@users.noreply.github.com>
2017-12-12 15:18:17 -0500

    Update HeaderView.js

--- src/client/components/HeaderView.js
+++ src/client/components/HeaderView.js
@@ -4,14 +4,14 @@ import React from 'react';
 import { AppBar, Navigation, Link } from 'react-toolbox';
 
 import Styles from '../assets/styles/app.css';
-// import logo from '../assets/images/transistor-logo.svg';
+import logo from '../assets/images/transistor-logo.svg';
 
 const HeaderView = () =>
   <AppBar title='Background Geolocation Console' leftIcon='menu'>
     <Navigation type='horizontal'>
-//       <Link href='http://transistorsoft.com' label=''>
+      <Link href='http://transistorsoft.com' label=''>
 //         <img className={Styles.logo} src={logo} />
-//       </Link>
+      </Link>
     </Navigation>
   </AppBar>;
 export default HeaderView;
